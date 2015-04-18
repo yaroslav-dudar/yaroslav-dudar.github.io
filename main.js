8f9512b7eb601aec6240642bca4c19c6eae32657
Yaroslav <flayingfog@gmail.com>
2015-04-18 10:29:12 +0300

    generate multiple blocks

--- main.js
+++ main.js
@@ -5,10 +5,16 @@ function init() {
 
     var height_count = 3;
     var width_count = 3;
+    // blocks on level
+    var blocks = [{w: 2, h: 2, color: "#fff", main: true},
+        {w: 1, h: 1, color: "#FFFF00", main: false},
+        {w: 1, h: 2, color: "#FFFF00", main: false},
+        {w: 1, h: 0, color: "#FFFF00", main: false}];
+
     // at the start of the game neither block is selected
     var selected_block = null;
-    // disable selecting any block during animation
-    // by default neither block selected
+    /* disable selecting any block during animation
+       by default neither block selected */
     var disable_select = false;
     // get canvas context
     var main_screen = new createjs.Stage("screen");
@@ -65,6 +71,7 @@ function init() {
                     // start move animation and handle when it ended
                     createjs.Tween.get(selected_block, {loop: false}).to({x: w, y: h}, 300).call(function() {
                         disable_select = false;
+                        // TODO: check if exists blocks that can be removed
                     });
                     var block_copy = selected_block;
                     // unselected block
@@ -78,44 +85,73 @@ function init() {
         }
     }
 
-    // render main block(position w: 2, h: 2)
-    var main_block = new createjs.Shape();
-    var w_pos_block = separator_w + separator_w * 2 + cellule_width * 2;
-    var h_pos_block = separator_h + separator_h * 2 + cellule_height * 2;
+    // render all blocks and add event hendlers
+    for (var i = 0; i < blocks.length; i++) {
 
-    main_block.graphics.beginFill("#fff").drawRoundRectComplex(w_pos_block,
-        h_pos_block, cellule_width, cellule_height, 10, 10, 10, 10);
-    // glow effect
-    main_block.shadow = new createjs.Shadow("#fff", 0, 0, cellule_width);
-    // check if exists default method to get x,y position!!
-    main_block.w_pos = 2;
-    main_block.h_pos = 2;
+        var block = new createjs.Shape();
 
-    main_screen.addChild(main_block);
+        var w_pos_block = separator_w + separator_w * blocks[i].w + cellule_width * blocks[i].w;
+        var h_pos_block = separator_h + separator_h * blocks[i].h + cellule_height * blocks[i].h;
 
-    main_block.on('click', function(event) {
-        selected_block = !this.graphics._stroke ? this: null;
-        // if block not selected
-        if (!this.graphics._stroke) {
-            if (disable_select) return;
-            this.graphics.clear().setStrokeStyle(5).beginStroke("#000");
-        } else {
-            this.graphics.clear();
-        }
-        // calculate block position in pixels
-        var w_pos_block = separator_w + separator_w * this.w_pos + cellule_width * this.w_pos;
-        var h_pos_block = separator_h + separator_h * this.h_pos + cellule_height * this.h_pos;
-
-        this.graphics.beginFill("#fff").drawRoundRectComplex(w_pos_block,
+        block.graphics.beginFill(blocks[i].color).drawRoundRectComplex(w_pos_block,
             h_pos_block, cellule_width, cellule_height, 10, 10, 10, 10);
-        this.x = 0; this.y = 0;
-        main_screen.update();
-    });
+        // glow effect
+        block.shadow = new createjs.Shadow(blocks[i].color, 0, 0, cellule_width / 10);
+
+        block.w_pos = blocks[i].w;
+        block.h_pos = blocks[i].h;
+
+        main_screen.addChild(block);
+
+        block.on('click', function(event) {
+            if (selected_block) {
+                if (!(selected_block === this)) {
+                    // select another block, and deselect current
+                    selected_block.dispatchEvent('click');
+                }
+            }
+
+            selected_block = !this.graphics._stroke ? this: null;
+            // if block not selected
+            if (!this.graphics._stroke) {
+                if (disable_select) return;
+                this.graphics.clear().setStrokeStyle(5).beginStroke("#000");
+            } else {
+                this.graphics.clear();
+            }
+            // calculate block position in pixels
+            var w_pos_block = separator_w + separator_w * this.w_pos + cellule_width * this.w_pos;
+            var h_pos_block = separator_h + separator_h * this.h_pos + cellule_height * this.h_pos;
+            // shadow color the same as shape color
+            this.graphics.beginFill(this.shadow.color).drawRoundRectComplex(w_pos_block,
+                h_pos_block, cellule_width, cellule_height, 10, 10, 10, 10);
+            this.x = 0; this.y = 0;
+            main_screen.update();
+        });
+    }
+
     main_screen.update();
 
     // settings for animation
     createjs.Ticker.addEventListener('tick', main_screen);
     createjs.Ticker.setFPS(60);
+    // note:every block has shadow
+    var candidates_w = main_screen.children.filter(function(elem){
+        return elem.shadow && elem.w_pos == 1 && elem.shadow.color == '#FFFF00';
+    }).sort(function(a, b) {return a.h_pos - b.h_pos}).reduce(function(prev, current, index, arr) {
+        if (prev.length == 3) return prev;
+        if (prev.length == 0) {
+            prev.push(current); return prev;
+        } else {
+            if (prev[prev.length - 1].h_pos + 1 == current.h_pos) {
+                prev.push(current); return prev;
+            } else {
+                return [];
+            }
+        }
+    }, []);
+    //main_screen.removeChild.apply(main_screen, candidates_w);
+    console.log(candidates_w);
 }
 
 function is_moved_block(shift_w, shift_h) {
