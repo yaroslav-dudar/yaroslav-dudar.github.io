dda005d7eb6866924899ae03cdb442fc24216f24
Yaroslav <flayingfog@gmail.com>
2015-04-21 22:37:50 +0300

    add restart action

--- main.js
+++ main.js
@@ -5,12 +5,13 @@ function init() {
 
     var height_count = 3;
     var width_count = 3;
-    // blocks on level
-    var blocks = [{w: 2, h: 2, color: "#fff", main: true},
-        {w: 0, h: 0, color: "#FFFF00", main: false},
-        {w: 1, h: 1, color: "#FFFF00", main: false},
-        {w: 2, h: 0, color: "#FFFF00", main: false}];
-
+    // blocks on level. main block should be only one
+    var blocks = [{w: 2, h: 2, color: "#fff", is_main: false},
+        {w: 0, h: 0, color: "#FFFF00", is_main: false},
+        {w: 1, h: 1, color: "#FFFF00", is_main: false},
+        {w: 2, h: 0, color: "#FFFF00", is_main: true}];
+    // variable with coordinates for destination(finish) cellue
+    var destination_cellule = {w: 0, h: 1};
     // at the start of the game neither block is selected
     var selected_block = null;
     /* disable selecting any block during animation
@@ -32,7 +33,7 @@ function init() {
     var cellule_width = (field_width - separator_w * (width_count + 1)) / width_count;
     var cellule_height = (field_height - separator_h * (height_count + 1)) / height_count;
 
-    // generate cellules
+    // render game field
     for (var i = 0; i < height_count; i++) {
         for (var j = 0; j < width_count; j++) {
 
@@ -40,7 +41,9 @@ function init() {
             var h_pos = separator_h + separator_h * i + cellule_height * i;
 
             var cellule = new createjs.Shape();
-            cellule.graphics.beginFill("#aaa").drawRoundRectComplex(w_pos,
+            // get cellue color
+            var cellule_color = destination_cellule.h == i && destination_cellule.w == j ? '#606060': "#aaa"; 
+            cellule.graphics.beginFill(cellule_color).drawRoundRectComplex(w_pos,
                 h_pos, cellule_width, cellule_height, 10, 10, 10, 10);
 
             main_screen.addChild(cellule);
@@ -54,7 +57,7 @@ function init() {
                     var shift_h = this.h_pos - selected_block.h_pos;
 
                     // check if block can be moved
-                    if (!is_moved_block(shift_w, shift_h)) {
+                    if (!is_block_can_moved(shift_w, shift_h)) {
                         // shake effect
                         createjs.Tween.get(selected_block, {loop: false})
                             .to({x: -5, y: 0}, 100)
@@ -70,19 +73,30 @@ function init() {
                     disable_select = true;
                     // start move animation and handle when it ended
                     createjs.Tween.get(selected_block, {loop: false}).to({x: w, y: h}, 300).call(function() {
-                        var count_successively = 3
-                        var w_candidates = search_remove_blocks('w_pos', block_copy, count_successively);
-                        if (w_candidates.length == count_successively) {
-                            // TODO: remove animation
-                            main_screen.removeChild.apply(main_screen, w_candidates);
-                        } else {
-                            var h_candidates = search_remove_blocks('h_pos', block_copy, count_successively);
-                            if (h_candidates.length == count_successively) {
+                        // represent how match blocks with same color must be consecutive to disappear
+                        var count_successively = 3;
+
+                        var positions = ['h_pos', 'w_pos'];
+                        for (var i = 0; i < positions.length; i++) {
+                            var candidates = search_remove_blocks(positions[i], block_copy, count_successively);
+                            if (candidates.length == count_successively) {
+                                var is_game_lose = candidates.filter(function(block) {return block.is_main;}).length > 0;
                                 // TODO: remove animation
-                                main_screen.removeChild.apply(main_screen, h_candidates);
+                                main_screen.removeChild.apply(main_screen, candidates);
+                                if (is_game_lose) {
+                                    console.log('You lose. Main block destroied');
+                                    show_restart_screen();
+                                }
+                                break;
                             }
                         }
 
+                        // check destination cellule
+                        if (block_copy.is_main && destination_cellule.w == block_copy.w_pos
+                            && destination_cellule.h == block_copy.h_pos && !is_game_lose) {
+                            // TODO: level complete
+                            console.log('You win!');
+                        }
                         disable_select = false;
                     });
                     var block_copy = selected_block;
@@ -97,64 +111,19 @@ function init() {
         }
     }
 
-    // render all blocks and add event hendlers
-    for (var i = 0; i < blocks.length; i++) {
-
-        var block = new createjs.Shape();
-
-        var w_pos_block = separator_w + separator_w * blocks[i].w + cellule_width * blocks[i].w;
-        var h_pos_block = separator_h + separator_h * blocks[i].h + cellule_height * blocks[i].h;
-
-        block.graphics.beginFill(blocks[i].color).drawRoundRectComplex(w_pos_block,
-            h_pos_block, cellule_width, cellule_height, 10, 10, 10, 10);
-        // glow effect
-        block.shadow = new createjs.Shadow(blocks[i].color, 0, 0, cellule_width / 10);
-
-        block.w_pos = blocks[i].w;
-        block.h_pos = blocks[i].h;
-
-        main_screen.addChild(block);
-
-        block.on('click', function(event) {
-            if (selected_block) {
-                if (!(selected_block === this)) {
-                    // select another block, and deselect current
-                    selected_block.dispatchEvent('click');
-                }
-            }
-
-            selected_block = !this.graphics._stroke ? this: null;
-            // if block not selected
-            if (!this.graphics._stroke) {
-                if (disable_select) return;
-                this.graphics.clear().setStrokeStyle(5).beginStroke("#000");
-            } else {
-                this.graphics.clear();
-            }
-            // calculate block position in pixels
-            var w_pos_block = separator_w + separator_w * this.w_pos + cellule_width * this.w_pos;
-            var h_pos_block = separator_h + separator_h * this.h_pos + cellule_height * this.h_pos;
-            // shadow color the same as shape color
-            this.graphics.beginFill(this.shadow.color).drawRoundRectComplex(w_pos_block,
-                h_pos_block, cellule_width, cellule_height, 10, 10, 10, 10);
-            this.x = 0; this.y = 0;
-            main_screen.update();
-        });
-    }
-
-    main_screen.update();
-
+    render_blocks_for_current_level();
     // settings for animation
     createjs.Ticker.addEventListener('tick', main_screen);
     createjs.Ticker.setFPS(60);
+    main_screen.update();
 
     function search_remove_blocks(pos, block, count_successively) {
         // pos must be: w_pos or h_pos
         var reverse_pos = pos == 'h_pos' ? 'w_pos': 'h_pos';
 
         return main_screen.children.filter(function(elem){
-            // note:every block has shadow
-            return elem.shadow && elem[pos] == block[pos] && elem.shadow.color == block.shadow.color;
+            // note:every block has is_main property
+            return 'is_main' in elem && elem[pos] == block[pos] && elem.shadow.color == block.shadow.color;
         }).sort(function(a, b) {return a[reverse_pos] - b[reverse_pos]}).reduce(function(prev, current) {
             if (prev.length == count_successively) return prev;
             if (prev.length == 0) {
@@ -168,9 +137,89 @@ function init() {
             }
         }, []);
     }
+
+    function remove_all_blocks() {
+        var all_blocks = main_screen.children.filter(function(elem){
+            // note:every block has is_main property
+            return 'is_main' in elem
+        });
+        main_screen.removeChild.apply(main_screen, all_blocks);
+    }
+
+    function render_blocks_for_current_level() {
+        // render all blocks and add event hendlers
+        for (var i = 0; i < blocks.length; i++) {
+
+            var block = new createjs.Shape();
+
+            var w_pos_block = separator_w + separator_w * blocks[i].w + cellule_width * blocks[i].w;
+            var h_pos_block = separator_h + separator_h * blocks[i].h + cellule_height * blocks[i].h;
+
+            block.graphics.beginFill(blocks[i].color).drawRoundRectComplex(w_pos_block,
+                h_pos_block, cellule_width, cellule_height, 10, 10, 10, 10);
+            // glow effect
+            block.shadow = new createjs.Shadow(blocks[i].color, 0, 0, cellule_width / 10);
+
+            block.w_pos = blocks[i].w;
+            block.h_pos = blocks[i].h;
+            block.is_main = blocks[i].is_main;
+            main_screen.addChild(block);
+
+            block.on('click', function(event) {
+                if (selected_block) {
+                    if (!(selected_block === this)) {
+                        // select another block, and deselect current
+                        selected_block.dispatchEvent('click');
+                    }
+                }
+
+                selected_block = !this.graphics._stroke ? this: null;
+                // if block not selected
+                if (!this.graphics._stroke) {
+                    if (disable_select) return;
+                    this.graphics.clear().setStrokeStyle(5).beginStroke("#000");
+                } else {
+                    this.graphics.clear();
+                }
+                // calculate block position in pixels
+                var w_pos_block = separator_w + separator_w * this.w_pos + cellule_width * this.w_pos;
+                var h_pos_block = separator_h + separator_h * this.h_pos + cellule_height * this.h_pos;
+                // shadow color the same as shape color
+                this.graphics.beginFill(this.shadow.color).drawRoundRectComplex(w_pos_block,
+                    h_pos_block, cellule_width, cellule_height, 10, 10, 10, 10);
+                this.x = 0; this.y = 0;
+                main_screen.update();
+            });
+        }
+    }
+
+    function show_restart_screen() {
+        // blocking main screen
+        var block_screen = new createjs.Shape();
+        block_screen.graphics.beginFill("#fff").drawRoundRectComplex(0,
+            0, field_width, field_height, 10, 10, 10, 10);
+        block_screen.on('click', function(event) {});
+        block_screen.alpha = 0;
+        main_screen.addChild(block_screen);
+
+        createjs.Tween.get(block_screen).to({alpha: 0.7}, 500);
+
+        // create button dynamicly
+        var btn = document.createElement('button');
+        btn.setAttribute('id', 'btn');
+        btn.innerHTML = 'Restart';
+        document.getElementById('container').appendChild(btn);
+
+        btn.addEventListener('click', function() {
+            remove_all_blocks();
+            main_screen.removeChild(block_screen);
+            render_blocks_for_current_level();
+            document.getElementById('container').removeChild(this);
+        });
+    }
 }
 
-function is_moved_block(shift_w, shift_h) {
+function is_block_can_moved(shift_w, shift_h) {
     if (!(shift_h == 0 || shift_w == 0)) return false;
     if (!(shift_h >= -1 && shift_h <= 1)) return false;
     if (!(shift_w >= -1 && shift_w <= 1)) return false;
