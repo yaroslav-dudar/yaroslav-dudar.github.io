495fee8d9e6a8cb07443d3aacfa8152f80488aa0
yaroslav <flayingfog@gmail.com>
2015-07-24 18:34:20 +0300

    add mapbox

---
+++ map.js
@@ -0,0 +1,7 @@
+$(function(){ 
+    L.mapbox.accessToken = 'pk.eyJ1IjoieWR1ZGFyIiwiYSI6IjBmYWRiMzk5ZTljODRlNTc4ZGU2ZjZkNWY5YzRjNWMzIn0.QqtTC5sdENWbluDyZoJx4w';
+    var map = L.mapbox.map('mapbox', 'mapbox.streets')
+        .setView([40, -89.50], 6);
+        
+    omnivore.csv('Entity_CFCs.csv').addTo(map);
+});
