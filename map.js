d7576e361e2b6ac5f6741d59fa89d9bb7fadf1e6
Yaroslav <flayingfog@gmail.com>
2016-05-21 18:15:38 +0300

    clear project from unnecessary files

--- map.js
+++
@@ -1,7 +0,0 @@
-$(function(){ 
-    L.mapbox.accessToken = 'pk.eyJ1IjoieWR1ZGFyIiwiYSI6IjBmYWRiMzk5ZTljODRlNTc4ZGU2ZjZkNWY5YzRjNWMzIn0.QqtTC5sdENWbluDyZoJx4w';
-    var map = L.mapbox.map('mapbox', 'mapbox.streets')
-        .setView([40, -89.50], 6);
-
-    omnivore.csv('Entity_CFCs.csv').addTo(map);
-});
